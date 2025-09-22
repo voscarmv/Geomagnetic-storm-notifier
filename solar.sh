32c82dc55fecc31db531dcdfcfea31791f8fbe47
Oscar Mier <voscarmv@gmail.com>
2025-09-21 22:57:06 -0500

    Create solar.sh

---
+++ solar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -O ~/Documents/kpfl_L59E.txt https://xras.ru/txt/kpfl_L59E.txt
+
+cat ~/Documents/kpfl_L59E.txt | \
+sed '/^#/d;s/ //g;s/\...$//' | \
+awk -F'|' '{print $1,$4}' | \
+grep -e '[567890]$' | \
+while read line ; do
+    XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "Solar storm on $line"
+done
