1b6c8b2e1e4cec46bb8e6e8f0f9539b5cd38e6ba
Oscar Mier <voscarmv@gmail.com>
2025-10-01 18:10:03 -0500

    Update solar.sh

--- solar.sh
+++ solar.sh
@@ -5,6 +5,7 @@ wget -O ~/Documents/kpfl_L59E.txt https://xras.ru/txt/kpfl_L59E.txt
 cat ~/Documents/kpfl_L59E.txt | \
 sed '/^#/d;s/ //g;s/\...$//' | \
 awk -F'|' '{print $1,$4}' | \
+sed 's/ 0$/ 1/' | \
 grep -e '[567890]$' | \
 while read line ; do
     XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "Solar storm on $line"
